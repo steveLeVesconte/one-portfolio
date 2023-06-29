f952d81fd722ec5d6b04299b5d1d82f23ed5e43e
steveLeVesconte <SteveLeVesconte@Gmail.com>
2023-06-29 14:20:32 -0700

    added h-screen and min-h-fit to home section div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 import {Link} from 'react-scroll';
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">  {/*"name" is  for smooth scroll */}
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 min-h-fit">  {/*"name" is  for smooth scroll */}
  
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
       
