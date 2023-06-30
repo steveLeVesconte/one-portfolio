46d989dc4fdc5fb0acd78b14bb675e818b49ff42
steveLeVesconte <SteveLeVesconte@Gmail.com>
2023-06-30 06:57:16 -0700

    Portfolio progress

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div name="about"
-    className=" w-full  bg-gradient-to-b from-gray-800 to-black text-white"
+    className=" w-full  bg-gradient-to-b from-gray-800 to-black text-white md:h-screen" 
     >
       <div 
       className=" max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,18 +5,28 @@ import navbar from '../assets/portfolio/navbar.jpg';
 import reactParallax from '../assets/portfolio/reactParallax.jpg';
 import reactSmooth from '../assets/portfolio/reactSmooth.jpg';
 import reactWeather from '../assets/portfolio/reactWeather.jpg';
+import ceuCatalog from '../assets/portfolio/CeuCatelog2fixed.png';
+import goGame from '../assets/portfolio/Play-1024x647.png';
+import publicPayment from '../assets/portfolio/paymentsearch-1024x798.png';
+
+
 const Portfolio = () => {
 
     const portfolios =[
         {
             id:1,
-            src: arrayDestruct
+            src: ceuCatalog,
+            bt1Label: 'Live Public',
+            bt2Label: 'none',
+            bt1Link: 'https://ceucatalog.age.ohio.gov/',
+            
         },{
             id:2,
-            src: reactParallax
+            src: publicPayment
         },{
             id:3,
-            src:  navbar
+            src:  goGame,
+
         },{
             id:4,
             src: reactSmooth
@@ -31,21 +41,21 @@ const Portfolio = () => {
 
   return (
     <div name="portfolio"
-    className=' bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen' >
+    className=' bg-gradient-to-b from-black to-gray-800 w-full text-white ' >
         <div className=' max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className=' pb-8'>
                 <p className=' text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
                 <p className=' py-6'>Check out some of my work right here</p>
             </div>
-            <div className=' grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+            <div className='grid  grid-flow-row gap-8  md:grid-cols-2  '>
 
                 {portfolios.map(({id,src}) => 
                     (
                         <div key={id} className=' shadow-md shadow-gray-600 rounded-lg'>   
                         <img src={src} alt="" className=' rounded-md duration-200 hover:scale-105'/>
                         <div className='flex items-center justify-center '>
-                            <button className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                            <button className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                       {/*    {bt1Label &&  <button  className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>{bt1Label}</button> } */}
+                            <button className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Show me more</button>
                         </div>
                     </div>
   ))
